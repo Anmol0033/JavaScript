e1384ad9c83c5314ffb0e8b6102119ab213ca7c0
Anmol0033 <136916667+Anmol0033@users.noreply.github.com>
2026-07-04 04:56:17 +0000

    Learnt about dataTypes in js. int, string, null, undefined, object etc.

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,16 @@
+"use strict" // treat all js code as new version;
+
+/* 
+    1. Number -> int, bigInt etc;
+    2. String -> "abcd..";
+    3. Boolean -> true/false;
+    4. null -> It is a stand alone value, i.e. it means the variable is initialized and 
+                has been assigned a null value i.e. empty value;
+    5. undefined -> means that variable is initialized but the value is not assigned.
+    6. Symbol -> used to find uniqueness, used in react, etc;
+
+    7. Object ->
+*/
+console.log(typeof("Anmol"));
+console.log(typeof(null));
+console.log(typeof(undefined));
