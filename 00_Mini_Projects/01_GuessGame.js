c39547f1dbcd86c777db2d29f5e0785dd22b169e
Anmol Kamboj <anmolkamboj0033@gmail.com>
2026-07-07 12:32:15 +0530

    Built a guess game

--- 00_Mini_Projects/01_GuessGame.js
+++ 00_Mini_Projects/01_GuessGame.js
@@ -1,6 +1,64 @@
+const { type } = require('os');
 const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+
+const secret =  Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+let attempts = 1;
+
+console.log("Hello Welcome to the Guess Game");
+console.log("You can choose any number from 1-10");
+
+function startGame()
+{
+    console.log(`Attempt: ${attempts}`);
+    rl.question("Enter your guess (or type exit to quit): ", (guessStr) =>
+    {
+        if(attempts == 3)
+        {
+            console.log("Your chances are over, Please start over");
+            rl.close();
+            return;
+        }
+        if(guessStr.trim().toLowerCase() == "exit")
+        {
+            console.log("Game exited, Thankyou for Playing");
+            rl.close();
+            return;
+        }
+
+        let num = Number(guessStr);
+
+        if(num > 10 || num < 0)
+        {
+            console.log("Please enter a valid number");
+            startGame();
+            return;
+        }
+        attempts++; 
+
+        if(num > secret)
+        {
+            console.log("Try a small number");
+            startGame();
+            return;
+        }
+        else if(num < secret)
+        {
+            console.log("Try a big number");
+            startGame();
+            return;
+        }
+        else 
+        {
+            console.log("YAY! You Guessed Right");
+            rl.close();
+            return;
+        }
+    })
+}
+
+startGame();
